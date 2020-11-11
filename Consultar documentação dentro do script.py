12de8363c959818a432e5d57d8bdbcc14e904d6a
Joviano Silveira <falecom@joviano.com>
2020-11-11 07:18:41 -0300

    _

---
+++ Consultar documentação dentro do script.py
@@ -0,0 +1,13 @@
+def divide(a,b):
+    '''
+    soma de dois numeros
+    a = numerador será um número inteiro
+    b = denominador será um número inteiro
+    '''
+    return a/b if b > 0 else 0
+
+help(divide)
+
+    
+import pandas as pd
+help(pd.read_csv)
